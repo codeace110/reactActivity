bf3ef76d58bdd345762544ec30c52639e2fc134c
codeace110 <neri11032321@gmail.com>
2022-12-14 22:04:46 +0800

    dwadwa

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import hero from './hero';
+import login from './login';
+import register from './register';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Route exact path="/" Component={login} />
+        <Route exact path="/register" Component={register} />
+        <Route exact path="/hero" Component={hero} />
+      </BrowserRouter>
     );
   }
 }
